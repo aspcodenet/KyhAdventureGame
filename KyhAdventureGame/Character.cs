bf248408a07c4818dbfe86ed8144e74567b3d937
Stefan Holmberg <stefan.holmberg@systementor.se>
2022-11-15 14:58:12 +0100

    a

---
+++ KyhAdventureGame/Character.cs
@@ -0,0 +1,73 @@
+﻿namespace KyhAdventureGame;
+
+public class Character
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+
+    public string Action = "";
+    public string LastFacingDirection = "Left";
+    public int LastSpriteIndex = -1;
+
+    public Character()
+    {
+        X = 20;
+        Y = 20;
+    }
+
+    public string GetSprite()
+    {
+        if (string.IsNullOrEmpty(Action))
+        {
+            if (LastFacingDirection == "Left") return Next(Sprites.IdleLeft);
+            else return Next(Sprites.IdleRight);
+        }
+        if (Action == "Go")
+        {
+            if (LastFacingDirection == "Left") return Next(Sprites.RunLeft);
+            else return Next(Sprites.RunRight);
+        }
+
+        return "";
+    }
+
+    private string Next(string[] idleLeft)
+    {
+        if (LastSpriteIndex == -1) LastSpriteIndex = 0;
+        if (LastSpriteIndex == idleLeft.Length) LastSpriteIndex = 0;
+        LastSpriteIndex++;
+        return idleLeft[LastSpriteIndex-1];
+    }
+
+    public void MoveLeft()
+    {
+
+        if (X > 0)
+        {
+            X--;
+            if(LastFacingDirection != "Left" || Action != "Go")
+                LastSpriteIndex = -1;
+        }
+        Action = "Go";
+        LastFacingDirection = "Left";
+    }
+
+    public void MoveRight()
+    {
+        if (X < 40)
+        {
+            X++;
+            if (LastFacingDirection != "Right" || Action != "Go")
+                LastSpriteIndex = -1;
+        }
+        Action = "Go";
+        LastFacingDirection = "Right";
+    }
+
+    public void Idle()
+    {
+        if(Action != "")
+            LastSpriteIndex = -1;
+        Action = "";
+    }
+}
--- KyhAdventureGame/Game.cs
+++ KyhAdventureGame/Game.cs
@@ -1,4 +1,6 @@
-﻿using Figgle;
+﻿using System.Text;
+using Figgle;
+using KyhAdventureGame;
 using Spectre.Console;
 
 public class Game
@@ -16,10 +18,63 @@ public class Game
             return ;
         }
 
+        Console.CursorVisible = false;
+        Console.Clear();
+        var character = new Character();
+        character.Action = "";
         while (true)
         {
-            //Game running
+            Console.CursorVisible = false;
+            if (Console.KeyAvailable)
+            {
+                Console.CursorVisible = false;
+                var action = Console.ReadKey(true).KeyChar;
+                if (action == 'a')
+                    character.MoveLeft();
+                else if (action == 'd')
+                    character.MoveRight();
+            }
+            else
+                character.Idle();
+            DrawScene();
 
+            WriteAt(character.GetSprite(), character.X, character.Y);
+            System.Threading.Thread.Sleep(100);
+        }
+    }
+
+    private void DrawScene()
+    {
+        WriteAt(Sprites.Mountain, 0, 0);
+        WriteAt(Sprites.Mountain2, 7, 1);
+        WriteAt(Sprites.Mountain3, 14, 0);
+        WriteAt(Sprites.Mountains, 21, 1);
+        WriteAt(Sprites.Mountain, 28, 0);
+        WriteAt(Sprites.Mountain2, 35, 1);
+        WriteAt(Sprites.Mountain3, 42, 0);
+        WriteAt(Sprites.Mountains, 49, 1);
+        WriteAt(Sprites.Mountain, 56, 0);
+        WriteAt(Sprites.Mountain2, 63, 1);
+        WriteAt(Sprites.Mountain3, 70, 0);
+        WriteAt(Sprites.Mountains, 77, 1);
+
+
+        WriteAt(Sprites.Tree2,10,10);
+        WriteAt(Sprites.Tree2, 17, 10);
+        WriteAt(Sprites.Store, 40, 7);
+        WriteAt(Sprites.Store, 40, 7);
+
+
+
+    }
+
+    void WriteAt(string s, int x, int y)
+    {
+        foreach (var part in s.Split("\n"))
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(part);
+            y++;
         }
     }
 }
