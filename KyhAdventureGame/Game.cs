de953498c18219d8176d4d30eb2c922f47a48826
Stefan Holmberg <stefan.holmberg@systementor.se>
2022-11-15 09:43:07 +0100

    Add project files.

---
+++ KyhAdventureGame/Game.cs
@@ -0,0 +1,25 @@
+﻿using Figgle;
+using Spectre.Console;
+
+public class Game
+{
+    public void Run()
+    {
+        var text = FiggleFonts.Standard.Render("PIZZA QUEST");
+        AnsiConsole.Write(text);
+        var image = new CanvasImage("pizza.png");
+        image.MaxWidth(40);
+        AnsiConsole.Write(image);
+
+        if (!AnsiConsole.Confirm("Ready to start ??"))
+        {
+            return ;
+        }
+
+        while (true)
+        {
+            //Game running
+
+        }
+    }
+}
